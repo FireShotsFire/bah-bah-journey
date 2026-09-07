6750c280d45c7bcd1ddcabb2611643fb8a713e19
FireShotsFire <87022590+FireShotsFire@users.noreply.github.com>
2026-09-07 07:50:37 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const SUPABASE_URL = "https://fosgssaetxnvyjfiucoe.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://fosgssaetxnvyjfiucoe.supabase.co/";
 const SUPABASE_ANON_KEY = "sb_publishable_y55Q3YV6IdQOWqfb1av2pg_kYlSDU06";
